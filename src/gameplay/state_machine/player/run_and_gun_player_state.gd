11cb820b31a049cf729e2abd925b646b70cc64e6
Ivan <malyshev.ivan@gmail.com>
2024-11-16 15:23:15 +0100

    Dash

--- src/gameplay/state_machine/player/run_and_gun_player_state.gd
+++ src/gameplay/state_machine/player/run_and_gun_player_state.gd
@@ -2,8 +2,12 @@ class_name RunAndGunPlayerState
 extends PlayerState
 
 const GRENADE_DELAY := 2.0
+const DASH_DURATION := 0.25
+const DASH_RELOAD_TIME := 5.0 # TODO: take from biomats
+const DASH_STRENGTH := 5
 var _shooter_timer := 0.0
 var _grenade_timer := 0.0
+var _dash_reload_timer := 0.0
 
 
 func enter():
@@ -24,6 +28,7 @@ func physics_update(delta: float):
 	
 	_shooter_timer += delta
 	_grenade_timer += delta
+	_dash_reload_timer += delta
 	
 	var move_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if move_direction:
@@ -52,7 +57,18 @@ func physics_update(delta: float):
 		var parent := player.get_parent() as Node2D # $PlayerStuff
 		parent.add_child(grenade)
 		_grenade_timer = 0.0
+	
+	if Input.is_action_pressed("dash") and _dash_reload_timer >= DASH_RELOAD_TIME:
+		_dash() # <-- Calling async function here
+		_dash_reload_timer = 0.0
 
 
 func exit():
 	pass
+
+
+func _dash():
+	var original_speed := player.stats.speed
+	player.stats.speed *= DASH_STRENGTH
+	await get_tree().create_timer(DASH_DURATION).timeout
+	player.stats.speed *= original_speed
