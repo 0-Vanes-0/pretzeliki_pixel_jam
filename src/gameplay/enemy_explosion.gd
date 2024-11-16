bf1ac551f45dbae435b42a792ffedf28fc3a5a55
Dekir <dekir0007@gmail.com>
2024-11-16 20:03:05 +0300

    explosion damage and time fix

--- src/gameplay/enemy_explosion.gd
+++ src/gameplay/enemy_explosion.gd
@@ -4,10 +4,13 @@ extends Area2D
 
 @export var explosion_damage : float = 1
 
+## чтобы не нанести урон два раза
+var _damaged : bool = false
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not _damaged:
 		body.take_damage(explosion_damage)
+		_damaged = true
 
 
 func _on_timer_timeout() -> void:
