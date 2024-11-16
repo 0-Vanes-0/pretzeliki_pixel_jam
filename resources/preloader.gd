5faca33e7ab18239e5f696ffdc2357d1466341a9
Ivan <malyshev.ivan@gmail.com>
2024-11-16 20:00:54 +0100

    Root ability (WIP)

--- resources/preloader.gd
+++ resources/preloader.gd
@@ -7,9 +7,10 @@ extends Node
 @export var blaster_projectile: PackedScene
 @export var biomat: PackedScene
 @export var grenade: PackedScene
+@export var root_ability: PackedScene
 
 @export_group("UIs")
 
 
 func _ready() -> void:
-	assert(blaster_projectile and biomat and grenade)
+	assert(blaster_projectile and biomat and grenade and root_ability)
---
+++ src/gameplay/player/root_ability.gd
@@ -0,0 +1,48 @@
+class_name RootAbility
+extends Area2D
+
+const GRAVITY_DURATION := 1.0
+const GRAVITY_STRENGTH := 200.0
+const ROOT_DURATION := 1.0
+
+@export_group("Required Children")
+@export var coll_shape: CollisionShape2D
+
+var spawn_position: Vector2
+var _affected_enemies: Array[Enemy] = []
+
+
+static func create(spawn_position: Vector2) -> RootAbility:
+	var root_ability := Preloader.root_ability.instantiate() as RootAbility
+	assert(root_ability)
+	root_ability.spawn_position = spawn_position
+	return root_ability
+
+
+func _ready() -> void:
+	assert(coll_shape)
+	self.position = spawn_position
+	
+	self.body_entered.connect(
+			func(body: Node2D):
+				if body is Enemy:
+					_affected_enemies.append(body)
+	)
+	await Global.create_timer(0.1)
+	
+#	for enemy in _affected_enemies:
+#		var direction := (self.position - enemy.position).normalized()
+#		var force := direction * GRAVITY_STRENGTH
+#		enemy.velocity += force
+	
+#	await Global.create_timer(GRAVITY_DURATION)
+	
+#	for enemy in _affected_enemies:
+#		enemy.velocity = Vector2.ZERO
+#		enemy.can_move = false
+	
+#	await Global.create_timer(ROOT_DURATION)
+
+#	for enemy in _affected_enemies:
+#		pass
+#		enemy.can_move = true
