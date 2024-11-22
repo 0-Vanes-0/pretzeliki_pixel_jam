3713698b235ea7bb6c4e7928716d7885c0a375c9
Ivan <malyshev.ivan@gmail.com>
2024-11-22 23:51:15 +0100

    Blaster reload

--- src/gameplay/state_machine/player/run_and_gun_player_state.gd
+++ src/gameplay/state_machine/player/run_and_gun_player_state.gd
@@ -5,14 +5,15 @@ const GUNSHOT_SOUND := "{20de73e4-98d8-4a26-9a37-f3490550498d}"
 const DASH_SOUND := "{5ac49b05-6976-4ec8-9418-7e759066d149}"
 
 const GRENADE_RELOAD_TIME := 2.0
-
 const DASH_DURATION := 0.1
 const DASH_STRENGTH := 5
+const ONE_BLASTER_RELOAD_TIME := 0.2
 
 var _shooter_timer := 0.0
 var _grenade_timer := 0.0
 var _dash_reload_timer := 0.0
 var _stun_reload_timer := 0.0
+var _blaster_reload_timer := 0.0
 
 
 func enter():
@@ -21,6 +22,7 @@ func enter():
 	_grenade_timer = GRENADE_RELOAD_TIME
 	_dash_reload_timer = player.stats.dash_reload_time
 	_stun_reload_timer = player.stats.stun_reload_time
+	_blaster_reload_timer = ONE_BLASTER_RELOAD_TIME * player.stats.max_ammo
 	#aim.activate() ??
 
 
@@ -38,6 +40,7 @@ func physics_update(delta: float):
 	_grenade_timer += delta
 	_dash_reload_timer += delta
 	_stun_reload_timer += delta
+	_blaster_reload_timer += delta
 	
 	var move_direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if move_direction:
@@ -51,7 +54,7 @@ func physics_update(delta: float):
 	else:
 		player.sprite.play(player.Animations.IDLE)
 	
-	if Input.is_action_pressed("shoot") and player.stats.current_ammo > 0 and _shooter_timer >= player.shoot_rate_time:
+	if Input.is_action_pressed("shoot") and player.stats.current_ammo > 0 and _shooter_timer >= player.shoot_rate_time and _blaster_reload_timer >= ONE_BLASTER_RELOAD_TIME * player.stats.max_ammo:
 		var start_position := player.position + player.get_weapon_gunpoint()
 		var direction := (player.current_look_direction - player.get_weapon_gunpoint()).normalized()
 		var blaster_pro := BlasterProjectile.create(player.stats.current_blaster_damage, start_position, direction, Color.CRIMSON)
@@ -82,6 +85,11 @@ func physics_update(delta: float):
 		player.stats.did_stun.emit()
 
 
+func handle_input(event: InputEvent):
+	if event.is_action_pressed("reload_blaster") and player.stats.current_ammo < player.stats.max_ammo and _blaster_reload_timer >= ONE_BLASTER_RELOAD_TIME * player.stats.max_ammo:
+		_reload()
+
+
 func exit():
 	pass
 
@@ -91,3 +99,13 @@ func _dash():
 	player.stats.speed *= DASH_STRENGTH
 	await Global.create_timer(DASH_DURATION)
 	player.stats.speed = original_speed
+
+
+func _reload():
+#	Global.play_sound("")
+	_blaster_reload_timer = 0.0
+	var ammo_delta := player.stats.max_ammo - player.stats.current_ammo
+	for i in ammo_delta:
+		player.stats.adjust_ammo(1)
+		await Global.create_timer(ONE_BLASTER_RELOAD_TIME)
+	_blaster_reload_timer = ONE_BLASTER_RELOAD_TIME * player.stats.max_ammo
